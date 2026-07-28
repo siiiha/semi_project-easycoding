d2b51ef383d49f52f2507c6cd4bca4c33b4c2fa5
healjoo422 <healjoo422@gmail.com>
2026-07-28 16:20:45 +0900

    storeEducation이 하나의 EducationDto 객체만 처리하도록 변경 이대량으로 처리하는 서비스 메서드를 별도로 정의하는것이 구조적으로 올바름

--- src/main/java/com/semi/easycoding/education/service/EducationService.java
+++ src/main/java/com/semi/easycoding/education/service/EducationService.java
@@ -6,15 +6,8 @@ import java.time.LocalDateTime;
 
 public interface EducationService {
 
-    String createEducations();
-    // 문제 생성 요청, API키를 이용해 LLM Agent에게 문제생성 요청후 반환
-    // json 타입으로 받을 것
-
-    EducationDto[] educationMapping(String response);
-    // 전달받은 데이터를 가공해 EducationDto[]로 변환 후 반환
-
-    boolean storeEducation(EducationDto[] educationList);
-    // 전달받은 EducationDto[]객체에 담긴 정보를 DB에 저장한다
+    boolean storeEducation(EducationDto myDto);
+    // 전달받은 EducationDto 객체에 담긴 정보를 DB에 저장한다
 
     EducationDto requestUserEducation(Long EducationID, Long userId);
     // DB에 저장된 문제풀에서 사용자에게 할당되지 않은 문제하나를 무직위로 할당하고 그 문제를 반환한다
