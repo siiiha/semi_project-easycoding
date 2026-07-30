ad69a3360f9e8ad5ed4db68a667a04eda81f21ef
healjoo422 <healjoo422@gmail.com>
2026-07-30 16:28:49 +0900

    해설 및 카테고리ID 부분 추가

--- src/main/java/com/semi/easycoding/education/dto/EducationDto.java
+++ src/main/java/com/semi/easycoding/education/dto/EducationDto.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 public class EducationDto {
     private Long educationId;
     private Short educationType;
+    private Short educationCategoryID;
     private String educationTitle;
     private String educationContent;
     private String educationExplanation;
