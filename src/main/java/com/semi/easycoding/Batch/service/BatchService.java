34227802b1ccb3f67aff846eb3e820cf2b602d15
healjoo422 <healjoo422@gmail.com>
2026-07-30 16:42:07 +0900

    전체를 문제를 n회 만큼 생성하는 배치서비스메서드 generateEducationsOption 정의 및 구현

---
+++ src/main/java/com/semi/easycoding/Batch/service/BatchService.java
@@ -0,0 +1,8 @@
+package com.semi.easycoding.Batch.service;
+
+import com.semi.easycoding.agent.service.AgentService;
+
+public interface BatchService {
+
+    void generateEducationsOption(int times);
+}
