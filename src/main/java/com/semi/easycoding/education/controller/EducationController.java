7da1c242d2f392508a60c521a56c7eaaca2fde91
healjoo422 <healjoo422@gmail.com>
2026-07-31 14:57:17 +0900

    daily_quiz 요청시 controller session에서 memberId 꺼낸다음 서비스에 넘겨주고 문제 리스트를 받아 모델에 추가 후 페이지 반환

--- src/main/java/com/semi/easycoding/education/controller/EducationController.java
+++ src/main/java/com/semi/easycoding/education/controller/EducationController.java
@@ -1,12 +1,17 @@
 package com.semi.easycoding.education.controller;
 
+import com.semi.easycoding.education.dto.EducationDto;
 import com.semi.easycoding.education.service.EducationService;
+import com.semi.easycoding.member.dto.MemberDto;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/education")
@@ -25,21 +30,29 @@ public class EducationController {
 
     // 일일 학습 페이지 이동
     @GetMapping("/daily-quiz")
-    public String dailyQuizPage(){
+    public String dailyQuizPage(HttpSession session, Model model){
         // 금일 할당된 학습을 조회한 다음, isEmpty() 여부에 따라서 분기
         // isEmpty() == true    : 신규 학습을 할당받도록 요청 후, 그 학습을 보여준다
         // isEmpty() == false   : 이미 할당받은 학습이 존재하므로, 그 학습을 그대로 보여준다
-        if(educationService.memberTodayEducationIsEmpty(1L)){ // 임시로 memberID를 1로 지정
-            //educationService.assignEducation();
-        }
 
-        return "/education/daily_quiz";
+        // todo 비로그인시, 처리로직 생각해야함
+
+        // 받아온 세션에서 memberID 꺼내서 Long타입으로 변환
+        MemberDto loginUser = (MemberDto) session.getAttribute("loginUser");
+        Long memberId = Long.valueOf(loginUser.getMemberId());
+
+
+        List<EducationDto> todayEducationList = educationService.todayEducations(memberId);
+
+        model.addAttribute("todayEducationList", todayEducationList);
+
+        return "education/daily_quiz";
     }
 
     // 카테고리 학습 페이지 이동
     @GetMapping("/category")
     public String categoryPage(){
-        return "/education/category";
+        return "education/category";
     }
 
     @GetMapping("/test")
