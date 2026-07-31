945913dbd86af1b639a94093397bc94ea2ba9354
healjoo422 <healjoo422@gmail.com>
2026-07-31 11:03:33 +0900

    컨트롤러 기능 정의(주석 추가)

--- src/main/java/com/semi/easycoding/education/controller/EducationController.java
+++ src/main/java/com/semi/easycoding/education/controller/EducationController.java
@@ -1,6 +1,5 @@
 package com.semi.easycoding.education.controller;
 
-import com.semi.easycoding.agent.ApiKeyValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,13 +20,17 @@ public class EducationController {
     // 일일 학습 페이지 이동
     @GetMapping("/daily-quiz")
     public String dailyQuizPage(){
-        return "education/daily_quiz";
+        // 금일 할당된 학습을 조회한 다음, isEmpty() 여부에 따라서 분기
+        // isEmpty() == true    : 신규 학습을 할당받도록 요청 후, 그 학습을 보여준다
+        // isEmpty() == false   : 이미 할당받은 학습이 존재하므로, 그 학습을 그대로 보여준다
+
+        return "/education/daily_quiz";
     }
 
     // 카테고리 학습 페이지 이동
     @GetMapping("/category")
     public String categoryPage(){
-        return "education/category";
+        return "/education/category";
     }
 
     @GetMapping("/test")
