ced463fed80727a67000ef18f65995519b8112bf
healjoo422 <healjoo422@gmail.com>
2026-07-31 11:38:17 +0900

    각 매핑별 주석추가

--- src/main/java/com/semi/easycoding/education/mapper/EducationMapper.java
+++ src/main/java/com/semi/easycoding/education/mapper/EducationMapper.java
@@ -13,9 +13,15 @@ import java.util.List;
 public interface EducationMapper {
 
     int insertQuiz(EducationDto myDto);
+    // 문제 삽입
     int insertOptions(EducationOptionTypeDto myDto);
+    // 객관식 답변 삽입
     int insertBlank(EducationBlankTypeDto myDto);
+    // 주관식 답변 삽입
     List<EducationCategoryDto> selectAllEduCategory();
+    // 모든 문제 카테고리 조회
     List<EducationDto> selectUserEducationAtDate(Long memberId, LocalDateTime startDate, LocalDateTime endDate);
+    // 특정 기간 동안 사용자에게 할당된 문제 조회
+
 
 }
