28db80d14a9040e13a2a0a2afe2300536dc15ba0
healjoo422 <healjoo422@gmail.com>
2026-08-03 15:21:15 +0900

    클래스명 좀더 직관적으로 변경

--- src/main/java/com/semi/easycoding/education/dto/MemberQuizHistoryDto.java
+++ src/main/java/com/semi/easycoding/education/dto/MemberQuizHistoryDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
-public class MemberQuizStatusDto {
+public class MemberQuizHistoryDto {
     Long historyId;
     Long educationId;
     boolean answered;
