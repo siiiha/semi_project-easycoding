feb55028736a7f7e756b1ca52d295c9c5ab60297
healjoo422 <healjoo422@gmail.com>
2026-08-11 14:16:10 +0900

    List<EducationOptionTypeSubmitDto>를 반환하는 서비스메서드

--- src/main/java/com/semi/easycoding/education/service/EducationServiceImp.java
+++ src/main/java/com/semi/easycoding/education/service/EducationServiceImp.java
@@ -324,5 +324,21 @@ public class EducationServiceImp implements EducationService {
     // 카테코리 id를 바탕으로 사용자에게 할당되지 않은 문제를 조회하여 할당
     // 문제 타입에 따라 답변까지 묶어서 반환
 
+    @Override
+    public List<EducationOptionTypeSubmitDto> getSubmittedEducationDtoAtDate(Long memberId, LocalDateTime startDate, LocalDateTime endDate){
+        List<EducationOptionTypeSubmitDto> submittedList = educationMapper.selectMemberQuizHistoryJoinAnsweredOptionJoinQuizAtDate(memberId, startDate, endDate);
+
+        // submittedList를 순회하면서 getAnswerByEducationId메서드를 이용해 OptionDto를 조회하고
+        // options에 대입
+        for (EducationOptionTypeSubmitDto submitted : submittedList) {
+            List<OptionDto> options = getAnswerByEducationId(submitted.getEducationId(), submitted.getEducationType());
+            submitted.setOptions(options);
+        }
+
+        return submittedList;
+    }
+    // 컨트롤러의 "/review" 요청을 받는 서비스 오케스트레이션 메서드
+    // 특정 기간동안 답변제출이 완료된 문제들을 조회하고
+    // 문제 타입에 따라서 답변과 히스토리 상태까지 묶어서 반환
 
 }
