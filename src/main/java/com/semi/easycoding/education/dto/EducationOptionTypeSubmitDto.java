c39c08d25306a3fc4200acc51a831574f73541e4
healjoo422 <healjoo422@gmail.com>
2026-08-11 09:09:05 +0900

    문제 다시보기에 필요한 복습용 DTO

---
+++ src/main/java/com/semi/easycoding/education/dto/EducationOptionTypeSubmitDto.java
@@ -0,0 +1,26 @@
+package com.semi.easycoding.education.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class EducationOptionTypeSubmitDto {
+    private Long educationId;
+    private Short educationType;
+    private Short educationCategoryID;
+    private String educationCategoryName;
+    private String educationTitle;
+    private String educationContent;
+    private String educationExplanation;
+    private List<OptionDto> options;
+    private Long historyId;
+    private boolean correct;
+    private Short choseOption;
+}
