2e9d76dbc72a1ae20af81b20c9ef3fb3d5822fa6
Kim-ilhyeon <kih3915@naver.com>
2026-08-12 09:38:11 +0900

    fix: add comment-profile

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -95,13 +95,17 @@ function reloadComment(comment) {
     profile.classList.add('profile');
     const profileImg = document.createElement('img');
     profileImg.classList.add('profile-img');
-    // 💡 프로필 이미지 경로 바인딩 (데이터에 없다면 기본이미지 처리)
-    // profile.src = comment.profileId || '/images/default-profile.png';
+    console.log('프로필 경로');
+    console.log(contextPath + '/images/profile/sheep-' + comment.profileId);
     // 프로필 이미지가 있을 경우
-    // if (comment.authorProfileImage) {
-    //     profileImg.src = comment.authorProfileImage;
-    //     profileImg.alt = comment.nickname;
-    // }
+    if (comment.profileId) {
+        profileImg.src = contextPath + '/images/profile/sheep-' + comment.profileId + '.png';
+        profileImg.alt = '프로필 이미지';
+    } else {
+        // 데이터가 없다면 기본이미지 처리
+        profileImg.src = contextPath + '/images/profile/sheep-1.png';
+        profileImg.alt = '프로필 이미지';
+    }
 
     // 작성자 영역
     const writerArea = document.createElement('div');
